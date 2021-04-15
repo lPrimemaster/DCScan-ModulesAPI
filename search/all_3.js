e13a40a9a105eac89fded86fa971925cc60b34b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-15 12:56:22 +0000

    Deploy lPrimemaster/DCScan-ModulesAPI to github.com/lPrimemaster/DCScan-ModulesAPI.git:gh-pages

--- search/all_3.js
+++ search/all_3.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['c_5fstr',['c_str',['../class_d_c_s_1_1_utils_1_1_string.html#ad191039e197ce19cb665d59027a2b4bb',1,'DCS::Utils::String']]],
+  ['channellimits',['ChannelLimits',['../struct_d_c_s_1_1_d_a_q_1_1_channel_limits.html',1,'DCS::DAQ']]],
   ['cmdbuffer',['CmdBuffer',['../class_d_c_s_1_1_coms_1_1_cmd_buffer.html',1,'DCS::Coms']]],
-  ['command',['Command',['../class_d_c_s_1_1_c_l_i_1_1_command.html',1,'DCS::CLI']]],
   ['command',['Command',['../struct_d_c_s_1_1_coms_1_1_command.html',1,'DCS::Coms']]],
+  ['command',['Command',['../class_d_c_s_1_1_c_l_i_1_1_command.html',1,'DCS::CLI']]],
   ['con_5fvalid',['CON_VALID',['../_d_c_s___network_2include_2internal_8h.html#a25cd817fa81601c18598508557c4d4e2',1,'CON_VALID():&#160;internal.h'],['../namespace_d_c_s_1_1_network_1_1_message.html#aa7033fbcfdb550b00135a10d3ebc45d3ade37cbc3e4855a4b40fd3ea7a0be58ad',1,'DCS::Network::Message::CON_VALID()']]],
   ['connect',['Connect',['../namespace_d_c_s_1_1_network_1_1_client.html#a4ee6eb135d9c3b3529c72f0b11c6cc83',1,'DCS::Network::Client']]],
   ['create',['Create',['../namespace_d_c_s_1_1_network_1_1_server.html#a0cc3b70261336d61074e07f36a1cb94a',1,'DCS::Network::Server']]],
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
+  ['channellimits',['ChannelLimits',['../struct_d_c_s_1_1_d_a_q_1_1_channel_limits.html',1,'DCS::DAQ']]],
   ['cmdbuffer',['CmdBuffer',['../class_d_c_s_1_1_coms_1_1_cmd_buffer.html',1,'DCS::Coms']]],
-  ['command',['Command',['../struct_d_c_s_1_1_coms_1_1_command.html',1,'DCS::Coms']]],
-  ['command',['Command',['../class_d_c_s_1_1_c_l_i_1_1_command.html',1,'DCS::CLI']]]
+  ['command',['Command',['../class_d_c_s_1_1_c_l_i_1_1_command.html',1,'DCS::CLI']]],
+  ['command',['Command',['../struct_d_c_s_1_1_coms_1_1_command.html',1,'DCS::Coms']]]
 ];
