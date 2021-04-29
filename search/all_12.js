54db739b52496c0312b266876926fdba6005aa46
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-29 11:53:29 +0000

    Deploy lPrimemaster/DCScan-ModulesAPI to github.com/lPrimemaster/DCScan-ModulesAPI.git:gh-pages

--- search/all_12.js
+++ search/all_12.js
@@ -5,5 +5,6 @@ var searchData=
   ['u64',['u64',['../_d_c_s___module_utils_8h.html#a0075e27a0ece07f1d9cf36c9109584ae',1,'DCS']]],
   ['u8',['u8',['../_d_c_s___module_utils_8h.html#a96d419836bdda13c4c329102842c3149',1,'DCS']]],
   ['unittarget',['UnitTarget',['../namespace_d_c_s_1_1_control.html#ad3e1fb34d05375e56c2219d9eb033e0a',1,'DCS::Control']]],
-  ['usbinthandle',['USBIntHandle',['../struct_d_c_s_1_1_u_serial_1_1_u_s_b_int_handle.html',1,'DCS::USerial']]]
+  ['usbinthandle',['USBIntHandle',['../struct_d_c_s_1_1_u_serial_1_1_u_s_b_int_handle.html',1,'DCS::USerial']]],
+  ['user',['User',['../struct_d_c_s_1_1_d_b_1_1_user.html',1,'DCS::DB']]]
 ];
--- search/classes_b.js
+++ search/classes_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['usbinthandle',['USBIntHandle',['../struct_d_c_s_1_1_u_serial_1_1_u_s_b_int_handle.html',1,'DCS::USerial']]]
+  ['usbinthandle',['USBIntHandle',['../struct_d_c_s_1_1_u_serial_1_1_u_s_b_int_handle.html',1,'DCS::USerial']]],
+  ['user',['User',['../struct_d_c_s_1_1_d_b_1_1_user.html',1,'DCS::DB']]]
 ];
