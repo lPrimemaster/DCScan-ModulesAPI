4a77c1f2d827f575fcd34731268a2908ad519d4c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-12 23:34:53 +0000

    Deploy lPrimemaster/DCScan-ModulesAPI to github.com/lPrimemaster/DCScan-ModulesAPI.git:gh-pages

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['pid',['PID',['../class_d_c_s_1_1_core_1_1_p_i_d.html',1,'DCS::Core']]],
   ['pipe_5fid',['PIPE_ID',['../struct_d_c_s_1_1_u_serial_1_1_p_i_p_e___i_d.html',1,'DCS::USerial']]]
 ];
